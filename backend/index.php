161331ed4e99e603d1d45cf71d76a1c06cf2299b
amnamk <amna.mutap@stu.ibu.edu.ba>
2025-06-05 14:09:43 +0200

    Deployment

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: https://sea-lion-app-8w9uk.ondigitalocean.app");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authentication");
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
