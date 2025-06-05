62916bd582e52f19d716c4bdad93424fbf321795
amnamk <amna.mutap@stu.ibu.edu.ba>
2025-06-05 13:07:29 +0200

    Deployment

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
 require_once __DIR__ . '/vendor/autoload.php';
