e00d5f8554cf7b53cbfdd5901eff0befefb7f428
amnamk <amna.mutap@stu.ibu.edu.ba>
2025-06-05 13:55:46 +0200

    Deployment

--- backend/index.php
+++ backend/index.php
@@ -1,10 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: https://sea-lion-app-8w9uk.ondigitalocean.app");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
 require_once __DIR__ . '/vendor/autoload.php';
